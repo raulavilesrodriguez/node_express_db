4879c6efccc85a398c29e69d212e21b735d2bdd5
Raul Aviles <finbravilesr@gmail.com>
2020-11-09 16:20:47 -0500

    se añadió pool chil tablas

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,16 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/lessons.db3'
     },
-    useNullAsDefault: true
-  }
+    //pool es el código para la relación entre la child tabla y la tabla padre
+    pool: {
+      afterCreate: (conn, done) =>{
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
   
 };
